a8dcfa6fa158210aaada7b07d97398bc88411625
Alecindro Steinke Castilho <alecindro.castilho@orsegups.com.br>
2020-06-18 10:33:50 -0300

    Initial version of keycloack_back generated by JHipster-6.9.1

---
+++ src/main/java/br/com/alecindro/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package br.com.alecindro.repository;
+
+import br.com.alecindro.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
